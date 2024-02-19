fd3ea4b2267463d2b535e90f8115000d848be9b0
sannabie <sksms0207@naver.com>
2024-02-19 16:37:51 +0900

    class -> className 속성 이름 변경

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -7,39 +7,39 @@ import '../css/style-xlarge.css'
 export default function Footer(){
     return(
         <div>
-        <footer id="footer"><div class="container">
-					<div class="row double">
-						<div class="6u">
-							<div class="row collapse-at-2">
-								<div class="6u">
+        <footer id="footer"><div className="container">
+					<div className="row double">
+						<div className="6u">
+							<div className="row collapse-at-2">
+								<div className="6u">
 									<h3>Accumsan</h3>
-									<ul class="alt"><li><a href="#">Nascetur nunc varius</a></li>
+									<ul className="alt"><li><a href="#">Nascetur nunc varius</a></li>
 										<li><a href="#">Vis faucibus sed tempor</a></li>
 										<li><a href="#">Massa amet lobortis vel</a></li>
 										<li><a href="#">Nascetur nunc varius</a></li>
 									</ul></div>
-								<div class="6u">
+								<div className="6u">
 									<h3>Faucibus</h3>
-									<ul class="alt"><li><a href="#">Nascetur nunc varius</a></li>
+									<ul className="alt"><li><a href="#">Nascetur nunc varius</a></li>
 										<li><a href="#">Vis faucibus sed tempor</a></li>
 										<li><a href="#">Massa amet lobortis vel</a></li>
 										<li><a href="#">Nascetur nunc varius</a></li>
 									</ul></div>
 							</div>
 						</div>
-						<div class="6u">
+						<div className="6u">
 							<h2>Aliquam Interdum</h2>
 							<p>Blandit nunc tempor lobortis nunc non. Mi accumsan. Justo aliquet massa adipiscing cubilia eu accumsan id. Arcu accumsan faucibus vis ultricies adipiscing ornare ut. Mi accumsan justo aliquet.</p>
-							<ul class="icons"><li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
-								<li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
-								<li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
-								<li><a href="#" class="icon fa-linkedin"><span class="label">LinkedIn</span></a></li>
-								<li><a href="#" class="icon fa-pinterest"><span class="label">Pinterest</span></a></li>
+							<ul className="icons"><li><a href="#" className="icon fa-twitter"><span className="label">Twitter</span></a></li>
+								<li><a href="#" className="icon fa-facebook"><span className="label">Facebook</span></a></li>
+								<li><a href="#" className="icon fa-instagram"><span className="label">Instagram</span></a></li>
+								<li><a href="#" className="icon fa-linkedin"><span className="label">LinkedIn</span></a></li>
+								<li><a href="#" className="icon fa-pinterest"><span className="label">Pinterest</span></a></li>
 							</ul></div>
 					</div>
 				</div>
 			</footer>
-            <div class="copyright">
+            <div className="copyright">
 			Powered by: <a href="https://templated.co/">TEMPLATED.CO</a>
 		    </div>
             </div>
--- src/component/Header.js
+++ src/component/Header.js
@@ -8,14 +8,14 @@ import '../css/style-xlarge.css'
 
 export default function Header(){
     return (
-        <header id="header" class="skel-layers-fixed"><h1><a href="#">Ion</a></h1>
+        <header id="header" className="skel-layers-fixed"><h1><a href="#">Ion</a></h1>
 				<nav id="nav">
                     <ul>
                         <li><a href="/">Home</a></li>
 						<li><a href="left-sidebar.html">학원소개</a></li>
 						<li><a href="right-sidebar.html">교육과정</a></li>
 						<li><a href="no-sidebar.html">메뉴1</a></li>
-						<li><a href="#" class="button special">Sign Up</a></li>
+						<li><a href="#" className="button special">Sign Up</a></li>
 					</ul>
                 </nav>
         </header>
