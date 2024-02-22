1fb2def2eedef52e4091a37534ae5c55c1e49494
hyeona-kim <hyeona1026@cu.ac.kr>
2024-02-22 10:16:21 +0900

    nav 수정

--- src/component/Nav.js
+++ src/component/Nav.js
@@ -8,12 +8,15 @@ import '../css/style-xlarge.css'
 export default function Nav(){
     return (
         <nav>
-            <section id="one" className="wrapper style1"><header className="major"><h2>Ipsum feugiat consequat</h2><br/>
-					<p>Tempus adipiscing commodo ut aliquam blandit</p>
-				</header><div className="container">
+            <section id="one" className="wrapper style1">
+				<header className="major">
+					<h2>국비지원취업과정</h2><br/>
+				</header>
+				<div className="container">
 					<div className="row">
 						<div className="4u">
-							<section className="special box"><i className="icon fa-area-chart major"></i>
+							<section className="special box">
+								<i className="icon fa-area-chart major"></i>
 								<h3>Justo placerat</h3>
 								<p>Eu non col commodo accumsan ante mi. Commodo consectetur sed mi adipiscing accumsan ac nunc tincidunt lobortis.</p>
 							</section></div>
@@ -30,68 +33,44 @@ export default function Nav(){
 					</div>
 				</div>
 			</section>
+
+
             <section id="two" className="wrapper style2">
-                <header className="major">
-                    <h2>Commodo accumsan aliquam</h2><br/>
-					<p>Amet nisi nunc lorem accumsan</p>
+				<header className="major">
+					<h2>완벽취업성공 프로그램</h2><br/>
 				</header>
                 <div className="container">
-					<div className="row">
-						<div className="6u">
-							<section className="special">
-                                <a href="#" className="image fit"><img src="../images/pic01.jpg" alt="logo" width="680" height="308"></img></a>
-								<h3>Mollis adipiscing nisl</h3>
-                                    <p>Eget mi ac magna cep lobortis faucibus accumsan enim lacinia adipiscing metus urna adipiscing cep commodo id.
-                                        Ac quis arcu amet. Arcu nascetur lorem adipiscing non faucibus odio nullam arcu lobortis. Aliquet ante feugiat. Turpis aliquet ac posuere volutpat lorem arcu aliquam lorem.
-                                    </p>
-								<ul className="actions">
-                                    <li>
-                                        <a href="#" className="button alt">Learn More</a>
-                                    </li>
-								</ul>
-                            </section>
-                        </div>
-						<div className="6u">
-							<section className="special">
-                                <a href="#" className="image fit"><img src="images/pic02.jpg" alt="logo2" width="680" height="308"/></a>
-								<h3>Neque ornare adipiscing</h3>
-                                    <p>Eget mi ac magna cep lobortis faucibus accumsan enim lacinia adipiscing metus urna adipiscing cep commodo id. Ac quis arcu amet.
-                                        Arcu nascetur lorem adipiscing non faucibus odio nullam arcu lobortis. Aliquet ante feugiat. Turpis aliquet ac posuere volutpat lorem arcu aliquam lorem.
-                                    </p>
-								<ul className="actions">
-                                    <li>
-                                        <a href="#" className="button alt">Learn More</a>
-                                    </li>
-								</ul>
+					<div className="row" >
+						<div className="12u" >
+							<section className="special ">
+                                <img src="../images/main-banner1.jpg" alt="logo" width="100%" height="720"></img>
+								<p>한국아이씨티기술협회 부설 한국ICT인재개발원에서는 수료 전부터 1:1 기업 취업연계 매칭으로 완벽 취업성공시스템을 진행합니다.<br/>
+									여러분은 취업에 대한 열정만 가지고 있으면 됩니다. 나머지 준비는 한국ICT인재개발원에서 모든 것을 준비합니다.<br/>
+									성공취업！ 그리고 취업 후에도 늘 여러분과 함께합니다.</p>
                             </section>
                         </div>
 					</div>
 				</div>
 			</section>
+
+
             <section id="three" className="wrapper style1">
                 <div className="container">
 					<div className="row">
-						<div className="8u">
-							<section><h2>Mollis ut adipiscing</h2>
+						<div className="6u">
+							<section>
+								<h2>캘린더 자리</h2>
 								<a href="#" className="image fit"><img src="images/pic03.jpg" alt="logo3" width="818" height="340"/></a>
 								<p>Vis accumsan feugiat adipiscing nisl amet adipiscing accumsan blandit accumsan sapien blandit ac amet faucibus aliquet placerat commodo. Interdum ante aliquet commodo accumsan vis phasellus adipiscing. Ornare a in lacinia. Vestibulum accumsan ac metus massa tempor. Accumsan in lacinia ornare massa amet. Ac interdum ac non praesent. Cubilia lacinia interdum massa faucibus blandit nullam. Accumsan phasellus nunc integer. Accumsan euismod nunc adipiscing lacinia erat ut sit. Arcu amet. Id massa aliquet arcu accumsan lorem amet accumsan commodo odio cubilia ac eu interdum placerat placerat arcu commodo lobortis adipiscing semper ornare pellentesque.</p>
 							</section>
                         </div>
-						<div className="4u">
+						<div className="6u">
 							<section>
-                                <h3>Magna massa blandit</h3>
+                                <h2>공지사항</h2>
 								<p>Feugiat amet accumsan ante aliquet feugiat accumsan. Ante blandit accumsan eu amet tortor non lorem felis semper. Interdum adipiscing orci feugiat penatibus adipiscing col cubilia lorem ipsum dolor sit amet feugiat consequat.</p>
 								<ul className="actions">
                                     <li><a href="#" className="button alt">Learn More</a></li>
 								</ul>
-                            </section><hr/>
-                            <section>
-                                <h3>Ante sed commodo</h3>
-                                    <ul className="alt"><li><a href="#">Erat blandit risus vis adipiscing</a></li>
-                                        <li><a href="#">Tempus ultricies faucibus amet</a></li>
-                                        <li><a href="#">Arcu commodo non adipiscing quis</a></li>
-                                        <li><a href="#">Accumsan vis lacinia semper</a></li>
-                                    </ul>
                             </section>
                         </div>
 					</div>
